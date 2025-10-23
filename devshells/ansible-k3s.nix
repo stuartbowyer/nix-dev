bf12f04d8c73feb0a46fb0d733c87f42cdaeed79
stuartbowyer <55706134+stuartbowyer@users.noreply.github.com>
2025-10-23 11:26:59 +0100

    Add MIT License to the project and sops/age

--- devshells/ansible-k3s.nix
+++ devshells/ansible-k3s.nix
@@ -8,7 +8,7 @@ forAllSystems (system:
     pkgs = nixpkgs.legacyPackages.${system};
   in
   {
-    ansible-k3s = pkgs.mkShellNoCC {
+    ansible-k3s = { kubeconfig ? "$PWD/secrets/.kubeconfig", sopsAgeKeyFile ? "$PWD/secrets/sops/age/keys.txt" }: pkgs.mkShellNoCC {
       name = "ansible-k3s-env-${system}";
       meta.description = "Development shell for managing K3s clusters with Ansible and FluxCD";
 
@@ -18,14 +18,18 @@ forAllSystems (system:
         ansible
         kubectl
         fluxcd
+        age
+        sops
       ];
 
       shellHook = ''
         # Use zsh as the default interactive shell
         export SHELL=${pkgs.zsh}/bin/zsh
 
-        # Point kubectl to local playbook config
-        export KUBECONFIG="$PWD/playbooks/.kubeconfig"
+        # Environment variables for kubeconfig and sops age key file
+        export KUBECONFIG="${kubeconfig}"
+        export SOPS_AGE_KEY_FILE="${sopsAgeKeyFile}"
+        export EDITOR="code --wait"
 
         # Only exec into zsh once per session
         if [ -t 1 ] && [ -z "$IN_ZSH" ]; then
