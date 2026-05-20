5ba0d116ec230ac0dca5db1f40bc30febd7b4d44
Stuart Bowyer <55706134+stuartbowyer@users.noreply.github.com>
2026-05-20 19:21:30 +0100

    Add python311 devShell with auto-managed .venv

--- devshells/default.nix
+++ devshells/default.nix
@@ -3,8 +3,10 @@
 let
   forAllSystems = nixpkgs.lib.genAttrs systems;
   ansibleK3sShells = import ./ansible-k3s.nix { inherit nixpkgs systems; };
+  python311Shells = import ./python311.nix { inherit nixpkgs systems; };
 in
 
 forAllSystems (system: {
   ansible-k3s = ansibleK3sShells.${system}.ansible-k3s;
+  python311 = python311Shells.${system}.python311;
 })
