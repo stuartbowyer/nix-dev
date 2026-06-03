5b549b947ae321849fe5f3fa5240688d75dd9ee0
Stuart Bowyer <55706134+stuartbowyer@users.noreply.github.com>
2026-06-03 16:51:17 +0100

    Remove ansible-k3s template (consume devShell directly)

--- flake.nix
+++ flake.nix
@@ -16,13 +16,5 @@
     {
       # Expose all reusable devShells
       devShells = devshells;
-
-      # Expose project templates (nix flake init -t ...)
-      templates = {
-        ansible-k3s = {
-          path = ./templates/ansible-k3s;
-          description = "Template for an Ansible + K3s + FluxCD project using nix-dev devShell";
-        };
-      };
     };
 }
